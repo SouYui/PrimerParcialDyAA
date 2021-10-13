c011e62443e8348242ebc8cacb26f9711e949e95
Sou Yui <luiscuriel911@aragon.unam.mx>
2021-10-13 12:08:05 -0500

    Problem 1 Docs

--- problemaUno.py
+++ problemaUno.py
@@ -1,6 +1,8 @@
 # Solución para el problema N° 1 de Diseño y Análisis de Algoritmos
 # Autor: SouYui
 
+# ! Requiere una lista de elementos tipo str
+# R Devuelve el indice del primer elemento que su predecesor es menor a él
 def primerElementoMenor(lista):
     if(type(lista) == list and len(lista) > 1):
         for i in range(1, len(lista)):
